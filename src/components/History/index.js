266ecb60faa8a8a5ecd7736e4594de6098e04eff
alanbbarros <alanbritobarros@gmail.com>
2021-12-01 01:50:31 -0300

    feat: add responsivity to the dashboard

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState, useContext} from 'react'
 import api from '../../services/api'
-import { format, parseISO } from 'date-fns'
+import { format, parseISO, getHours, getMinutes } from 'date-fns'
 import { Link } from 'react-router-dom'
 import { dataContext } from '../../context'
 import './history.css'
@@ -29,28 +29,42 @@ const History = () => {
                 <thead>
                     <tr>
                         <th scope='col' >Date</th>
-                        <th scope='col' >Status</th>
+                        <th scope='col' className='removable' >Status</th>
                         <th scope='col' >Patient</th>
-                        <th scope='col' >Type</th>
+                        <th scope='col' className='removable' >Type</th>
                     </tr>
                 </thead>
 
                 <tbody>
                     {appointmentsInfo.map((item, index) =>{
+
                         const today = new Date()
+
                         let data = parseISO(item.startTime) 
-                        let formatedData = format(data, "MM-dd-yyyy")
+                        let formatedData = format(data, "MM/dd/yyyy")
+
+                        let hoursStart = getHours(data)
+                        let minutesStart = getMinutes(data) ? getMinutes(data) : '00' // if the appointment starts at "X:00", transform 0 in 00 for a better display 
+
+                        let hoursEnd = item.endTime ? getHours(parseISO(item.endTime)) : null
+                        let minutesEnd = getMinutes(item.endTime) ? getMinutes(item.endTime) : '00'
+
+
                         if(data < today){
                             return(
                                 <tr className='table-row' key={index} >
                                     <td>
                                         <Link key={index} to={`/details/${item.patientId}`}>
-                                            {formatedData} 
+                                            {formatedData} {`${hoursStart}:${minutesStart}`} {hoursEnd ? `- ${hoursEnd}:${minutesEnd}` : ''}
                                         </Link>
                                     </td>
 
-                                    <td>
-                                        <Link key={index} to={`/details/${item.patientId}`}>
+                                    <td className='removable' >
+                                        <Link
+                                        className={item.status === 'completed' ? 'status-column completed' : (item.status === 'absent') ? 'status-column absent' : 'status-column'}
+                                        key={index}
+                                        to={`/details/${item.patientId}`}
+                                        >
                                             {item.status} 
                                         </Link>
                                     </td>
@@ -61,7 +75,7 @@ const History = () => {
                                         </Link>
                                     </td>
 
-                                    <td>
+                                    <td className='removable' >
                                         <Link key={index} to={`/details/${item.patientId}`}>
                                             {item.type} 
                                         </Link>
